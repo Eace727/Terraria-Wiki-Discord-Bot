4dda7fe80bef4449e898fad0f841b469373301a9
Eace727 <eace727@gmail.com>
2024-08-25 14:36:22 -0700

    reset

--- Main.py
+++ Main.py
@@ -23,12 +23,12 @@ async def on_message(message):
     if message.content.startswith('mega'):
         await message.channel.send('What a Cutie')
         await message.channel.send(file=discord.File('Mega.jpg'))
+    if message.content.startswith('karan'):
+        await message.channel.send('What a Cutie')
+        await message.channel.send(file=discord.File('Karan.jpg'))
     if message.content.startswith('eman'):
         await message.channel.send('What a Cutie')
         await message.channel.send(file=discord.File('Ethan.jpg'))
-    if message.content.startswith('estavel'):
-        await message.channel.send('What a Cutie')
-        await message.channel.send(file=discord.File('karan.jpg'))
     
 
 client.run('MTI3NzMzNjY5MTY1NjM2MDA0OA.GH-rq9.o88agbxuKiYs9VxVVsbGvcVqLDI_rMUSlTDLt4')
