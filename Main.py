b6045eb5d3fc9fb0fca59dfa6b209642aedf9719
Enrique Contreras <Eace727@gmail.com>
2024-08-27 17:16:35 -0400

    added shutdown function

--- Main.py
+++ Main.py
@@ -1,5 +1,6 @@
 import discord
 import os
+from discord.ext import commands
 from dotenv import load_dotenv
 
 load_dotenv()
@@ -7,7 +8,7 @@ load_dotenv()
 intents = discord.Intents.default()
 intents.message_content = True
 
-client = discord.Client(intents=intents)
+client = commands.Bot(command_prefix='-', intents=intents)
 
 @client.event
 async def on_ready():
@@ -17,7 +18,6 @@ async def on_ready():
 async def on_message(message):
     if message.author == client.user:
         return
-
     if message.content.startswith('$hello'):
         await message.channel.send('You should go Keep yourself safe NOW!')
         await message.channel.send(file=discord.File('Keepyourselfsafe.jpg'))
@@ -29,12 +29,13 @@ async def on_message(message):
     if message.content.startswith('eman'):
         await message.channel.send('What a Cutie')
         await message.channel.send(file=discord.File('Ethan.jpg'))
-
-        #this
     if message.content.startswith('estavel'):
         await message.channel.send('What a Cutie')
         await message.channel.send(file=discord.File('karan.jpg'))
-#This is crazy bro caint belive it
-    
+
+@client.command()
+async def shutdown(ctx):
+    await ctx.send("Shutting down...")
+    await client.close()
 
 client.run(os.getenv('TOKEN'))
