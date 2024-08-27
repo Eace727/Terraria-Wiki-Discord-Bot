1480c741a578d995d6c49c7e3ca8fd140936e586
Enrique Contreras <Eace727@gmail.com>
2024-08-27 19:55:51 -0400

    removed useless commands and images

--- Main.py
+++ Main.py
@@ -12,10 +12,17 @@ intents.message_content = True
 
 client = commands.Bot(command_prefix='!', intents=intents)
 
+@client.event
+async def on_ready():
+    await client.tree.sync()
+    print('We have logged in as {0.user}'.format(client))
+
+# sends pong when pinged
 @client.tree.command(name="ping", description="Check the bot's latency")
 async def ping(interaction: discord.Interaction):
     await interaction.response.send_message("Pong!")
 
+# searches the terraria wiki for the given search term
 @client.tree.command(name="search", description="search the terraria wiki")
 async def search_wiki(interaction: discord.Interaction, search: str):
     url = "https://terraria.wiki.gg/api.php"
@@ -42,36 +49,6 @@ async def search_wiki(interaction: discord.Interaction, search: str):
         message = "No results found."
 
     await interaction.response.send_message(message)
-
-
-
-@client.event
-async def on_ready():
-    print('We have logged in as {0.user}'.format(client))
-
-@client.event
-async def on_message(message):
-    if message.author == client.user:
-        return
-    if message.content.startswith('$hello'):
-        await message.channel.send('You should go Keep yourself safe NOW!')
-        await message.channel.send(file=discord.File('Keepyourselfsafe.jpg'))
-    if message.content.startswith('$dog'):
-        await message.channel.send(file=discord.File('Dog.jpg'))
-    if message.content.startswith('mega'):
-        await message.channel.send('What a Cutie')
-        await message.channel.send(file=discord.File('Mega.jpg'))
-    if message.content.startswith('eman'):
-        await message.channel.send('What a Cutie')
-        await message.channel.send(file=discord.File('Ethan.jpg'))
-    if message.content.startswith('estavel'):
-        await message.channel.send('What a Cutie')
-        await message.channel.send(file=discord.File('karan.jpg'))
-
-@client.event
-async def on_ready():
-    await client.tree.sync()
-    print(f'Logged in as {client.user}!')
 
 
 client.run(os.getenv('TOKEN'))
